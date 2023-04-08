178836a31e04dede0c632e1957fb1456f81bfb6a
rcdavis <rcdavisgd@gmail.com>
2023-04-08 12:07:50 -0400

    Added TreeView to fxml

--- src/main/kotlin/com/example/javafxtest/HelloApplication.kt
+++ src/main/kotlin/com/example/javafxtest/HelloApplication.kt
@@ -2,18 +2,24 @@ package com.example.javafxtest
 
 import javafx.application.Application
 import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
 import javafx.scene.Scene
-import javafx.scene.layout.VBox
 import javafx.stage.Stage
 
 class HelloApplication : Application() {
     override fun start(stage: Stage) {
-        val jclass = HelloApplication::class.java;
-        val fxmlLoader = FXMLLoader(jclass.getResource("hello-view.fxml"))
-        val scene = Scene(fxmlLoader.load(), 320.0, 240.0)
+        val fxmlLoader = FXMLLoader(javaClass.getResource("hello-view.fxml"))
+        val root = fxmlLoader.load<Parent>()
+        //val controller = fxmlLoader.getController<HelloController>()
+
         stage.title = "Hello!"
-        stage.scene = scene
+        stage.scene = Scene(root, width, height)
         stage.show()
+    }
+
+    companion object {
+        private const val width = 320.0
+        private const val height = 240.0
     }
 }
 
--- src/main/kotlin/com/example/javafxtest/HelloController.kt
+++ src/main/kotlin/com/example/javafxtest/HelloController.kt
@@ -2,13 +2,24 @@ package com.example.javafxtest
 
 import javafx.fxml.FXML
 import javafx.scene.control.Label
+import javafx.scene.control.TreeItem
+import javafx.scene.control.TreeView
 
 class HelloController {
     @FXML
-    private lateinit var welcomeText: Label
+    private lateinit var treeView: TreeView<String>
 
     @FXML
-    private fun onHelloButtonClick() {
-        welcomeText.text = "Welcome to JavaFX Application!"
+    private fun onAddTreeItemClick() {
+        addTreeItem("New Node ${treeView.root.children.size}")
+    }
+
+    @FXML
+    private fun onRemoveTreeItemClick() {
+
+    }
+
+    private fun addTreeItem(s: String) {
+        treeView.root.children.add(TreeItem(s))
     }
 }
